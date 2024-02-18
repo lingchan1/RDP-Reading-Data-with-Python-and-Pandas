d6d42da6103b766bbe4917b4ce0e64300f64a48c
lingchan1 <108247513+lingchan1@users.noreply.github.com>
2024-02-18 20:23:46 +0900

    Update main.py

--- unit-1-reading-data-with-python-and-pandas/lesson-2-load-movies-dataset/main.py
+++ unit-1-reading-data-with-python-and-pandas/lesson-2-load-movies-dataset/main.py
@@ -4,4 +4,11 @@ import pandas as pd
 column_names = ['color', 'director_name', 'num_critic_for_reviews', 'duration',
                 'gross', 'movie_title', 'num_user_for_reviews',	'country',
                 'cotent_rating', 'budget', 'title_year', 'imdb_score', 'genre']
+movies = pd.read_csv('movies.csv', 
+                     header=None, 
+                     names=column_names,
+                     sep='|',
+                     skiprow=[1,2,3])
+movies.head()
 
+                     
